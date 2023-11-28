229cbef7deefa7f63bb3f6158f7dfd3ebed1ca06
Shashank Naik <shashanknaik808@gmail.com>
2023-11-28 16:36:31 +0530

    Create a basic React component with 'Hello World'

---
+++ mui-tut/src/Test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { } from '@mui/material';
+
+function Test() {
+    return (
+        <div>
+            Hello World
+        </div>
+    )
+}
+
+export default Test;
