45e331bba43053d09d0c9dbdabf36411bd6f1e2e
Shashank Naik <shashanknaik808@gmail.com>
2023-11-28 16:57:24 +0530

    Installing Material UI Package

--- mui-tut/src/Test.jsx
+++ mui-tut/src/Test.jsx
@@ -1,25 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, TextField } from '@mui/material';
 
 function Test() {
-
-    const [inputs, setInputs] = useState({
-        name: "",
-        email: "",
-        password: ""
-    });
-
-    function handleChange(e) {
-        setInputs((prevState) => ({
-            ...prevState,
-            [e.target.name]: e.target.value
-        }));
-    }
-
-    function handleSubmit(e) {
-        e.preventDefault();
-        console.log(inputs);
-    }
 
     return (
         <div>
@@ -29,7 +11,7 @@ function Test() {
                     sx={{ margin: 3 }} placeholder='Name' variant='outlined' />
                 <TextField name='email' value={inputs.email}
                     onChange={handleChange} type={'email'}
-                    sx={{ margin: 3 }} placeholder='Email' variant='standard' />
+                    sx={{ margin: 3 }} placeholder='Email' variant='outlined' />
                 <TextField name='password' value={inputs.password}
                     onChange={handleChange} type={'password'}
                     sx={{ margin: 3 }} placeholder='Password' variant='filled' />
