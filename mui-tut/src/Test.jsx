ad4f1dc43103773b92ceb8efaf498f8e3064ff37
Shashank Naik <shashanknaik808@gmail.com>
2023-11-28 16:39:34 +0530

    Added a form with name, email, and password fields

--- mui-tut/src/Test.jsx
+++ mui-tut/src/Test.jsx
@@ -4,7 +4,19 @@ import { } from '@mui/material';
 function Test() {
     return (
         <div>
-            Hello World
+            <form onSubmit={handleSubmit}>
+                <TextField name='name' value={inputs.name}
+                    onChange={handleChange} type={'text'}
+                    sx={{ margin: 3 }} placeholder='Name' variant='outlined' />
+                <TextField name='email' value={inputs.email}
+                    onChange={handleChange} type={'email'}
+                    sx={{ margin: 3 }} placeholder='Email' variant='standard' />
+                <TextField name='password' value={inputs.password}
+                    onChange={handleChange} type={'password'}
+                    sx={{ margin: 3 }} placeholder='Password' variant='filled' />
+
+                <Button type='submit'>Submit</Button>
+            </form>
         </div>
     )
 }
